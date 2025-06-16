35dfb1ee8f8425be6f38faaf2a40c773701f274b
@Khushi21 <bhopishrutika21@gmail.com>
2025-06-16 17:23:58 +0100

    Clean code and testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router'
 import './App.css'
 
 function App() {
   
   return (
    <>
-    <h1>Welcome to My React App</h1>
-    <p>This is a simple React application.</p>
-    <p>Feel free to explore and modify the code!</p>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<h1>KanBan board</h1>}></Route>
+    </Routes>
+  </BrowserRouter>
+  <div className='Header'>
+  <h1>Welcome to the Kanban Board</h1>
+  </div>
+
+
    </>
   )
 }
