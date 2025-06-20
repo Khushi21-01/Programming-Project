2bc91cca4d5963f0b445a3dfc0b6c30f59670d6e
@Khushi21 <bhopishrutika21@gmail.com>
2025-06-20 11:36:36 +0100

    Working on api endpoint for server in index.js

--- kanban-board-backend/index.js
+++ kanban-board-backend/index.js
@@ -9,6 +9,9 @@ app.use(cors());
 
 app.use(express.json());
 app.use("/api/tasks", taskRoutes);
+app.get('/', (req, res) => {
+  res.send('Welcome to Kanban Board API');
+});
 
   app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
