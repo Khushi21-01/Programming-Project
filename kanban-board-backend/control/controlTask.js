9e3cd69d4ca66348711577f349df1de2cd8e5941
@Khushi21 <bhopishrutika21@gmail.com>
2025-06-27 20:28:57 +0100

    updated controltask file for read operation. To get the the task by its Id

--- kanban-board-backend/control/controlTask.js
+++ kanban-board-backend/control/controlTask.js
@@ -1,6 +1,21 @@
 import TaskModel from '../Models/Tasks.js';
 
 export default class TaskController {
+    // to get the tasks by its ID read operation
+  async getTaskById(req, res) {
+        const taskId  = req.params.taskId;
+        try{
+             const requestedtask = await TaskModel.getTaskById(taskId);
+             if(!requestedtask) {
+                return res.status(404).json({ message: "Task not found" });
+             }
+             res.status(200).json({message: "Task retrieved successfully", task: requestedtask}); 
+             } catch (error){
+                res.status(500).json({message: error.message});
+            }
+        }
+
+
     // Get tasks by section
     async getTasks(req, res) {
         const { section } = req.params;
@@ -13,7 +28,7 @@ export default class TaskController {
     }
 
     // Add a new task
-    async addTask(req, res) {
+ async addTask(req, res) {
         // const { name, description, dueDate, assignee, section } = req.body;
         try {
             const task = await TaskModel.addTask(req.body);
@@ -21,29 +36,34 @@ export default class TaskController {
             if (!task) {
                 return res.status(400).json({ message: "Failed to add task" });
             }
-
-            res.status(201).json({ message: "Task added successfully", task });
+            const newtask= await task.save();
+            
+            res.status(201).json({ message: "Task added successfully", newtask});
         } catch (err) {
             res.status(400).json({ message: err.message });
         }
     }
 
-
+ 
     // Update a task
     async updateTask(req, res) {
-        const { taskId } = req.params;
+        const taskId  = req.params.taskId;
         const updatedTask = req.body;
 
         try {
             const task = await TaskModel.updateTask(taskId, updatedTask);
-            res.status(200).json({ message: "Task updated successfully", task });
+            const ressave = await task.save();
+            if (!task) {
+                return res.status(404).json({ message: "Task not found" });
+            }
+            res.status(200).json({ message: "Task updated successfully", ressave });
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
     }
 
     // Move task to another section
-    async moveTask(req, res) {
+ async moveTask(req, res) {
         const { taskId, sourceSectionId, destinationSectionId } = req.body;
         try {
             const task = await TaskModel.moveTask(taskId, sourceSectionId, destinationSectionId);
@@ -54,7 +74,7 @@ export default class TaskController {
     }
 
     // Delete a task
-    async deleteTask(req, res) {
+ async deleteTask(req, res) {
         const { taskId } = req.params;
 
         try {
